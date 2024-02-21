d58b1ef78d3e62954ebd7746f4c709e7c950cc01
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-02-21 11:20:02 +0200

    Generated Backend Spring Boot App

---
+++ backend/src/main/java/lt/scoutress/StatisticsApp/StatisticsAppApplication.java
@@ -0,0 +1,13 @@
+package lt.scoutress.StatisticsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisticsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatisticsAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lt/scoutress/StatisticsApp/StatisticsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.scoutress.StatisticsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

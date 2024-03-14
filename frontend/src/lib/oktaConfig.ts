0e7fc67113402d4bd2cf7fd92bd7b172748d67c6
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-03-14 10:36:31 +0200

    Removed unnecessary files

--- frontend/src/lib/oktaConfig.ts
+++
@@ -1,8 +0,0 @@
-export const oktaConfig = {
-    clientId: '0oaf9y63nslf8OovQ5d7',
-    issuer: 'https://dev-48534325.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
-    scopes: ['openid', 'profile', 'email'],
-    pkce: true,
-    disableHttpsCheck: true,
-}

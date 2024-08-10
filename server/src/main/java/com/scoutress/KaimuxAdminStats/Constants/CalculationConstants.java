45ff444782928b381f105a6ea6ed983e2f5cc12c
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-08-10 12:53:56 +0300

    Added constants

---
+++ server/src/main/java/com/scoutress/KaimuxAdminStats/Constants/CalculationConstants.java
@@ -0,0 +1,47 @@
+package com.scoutress.KaimuxAdminStats.Constants;
+
+public class CalculationConstants {
+    
+  public static final double SERVER_TICKETS_SUPPORT = 2.0;
+  public static final double SERVER_TICKETS_CHATMOD = 1.0;
+  public static final double SERVER_TICKETS_OVERSEER = 0.5;
+  public static final double SERVER_TICKETS_ORGANIZER = 0.5;
+  public static final double SERVER_TICKETS_MANAGER = 0.25;
+
+  public static final double SERVER_TICKETS_PERC_SUPPORT = 5.0;
+  public static final double SERVER_TICKETS_PERC_CHATMOD = 2.5;
+  public static final double SERVER_TICKETS_PERC_OVERSEER = 1.17;
+  public static final double SERVER_TICKETS_PERC_ORGANIZER = 1.17;
+  public static final double SERVER_TICKETS_PERC_MANAGER = 1.0;
+
+  // Not tested yet
+  // public static final double DISCORD_TICKETS_SUPPORT = 2.0;
+  // public static final double DISCORD_TICKETS_CHATMOD = 1.0;
+  // public static final double DISCORD_TICKETS_OVERSEER = 0.5;
+  // public static final double DISCORD_TICKETS_ORGANIZER = 0.5;
+  // public static final double DISCORD_TICKETS_MANAGER = 0.25;
+
+  // public static final double DISCORD_TICKETS_PERC_SUPPORT = 5.0;
+  // public static final double DISCORD_TICKETS_PERC_CHATMOD = 2.5;
+  // public static final double DISCORD_TICKETS_PERC_OVERSEER = 1.17;
+  // public static final double DISCORD_TICKETS_PERC_ORGANIZER = 1.17;
+  // public static final double DISCORD_TICKETS_PERC_MANAGER = 1.0;
+
+  public static final double PLAYTIME_HELPER = 2.0;
+  public static final double PLAYTIME_SUPPORT = 1.0;
+  public static final double PLAYTIME_CHATMOD = 0.5;
+  public static final double PLAYTIME_OVERSEER = 0.25;
+  public static final double PLAYTIME_ORGANIZER = 0.25;
+  public static final double PLAYTIME_MANAGER = 0.125;
+
+  // public static final double AFK_PLAYTIME_HELPER = 2.0;
+  // public static final double AFK_PLAYTIME_SUPPORT = 1.0;
+  // public static final double AFK_PLAYTIME_CHATMOD = 0.5;
+  // public static final double AFK_PLAYTIME_OVERSEER = 0.25;
+  // public static final double AFK_PLAYTIME_ORGANIZER = 0.25;
+  // public static final double AFK_PLAYTIME_MANAGER = 0.125;
+
+  public static final double COMPLAINT = 0.01;
+
+  private CalculationConstants() {}
+}

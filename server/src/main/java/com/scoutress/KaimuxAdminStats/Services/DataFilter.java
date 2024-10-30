d39a62defecffcffeeda16002b03f367469949bc
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-10-30 20:01:24 +0200

    Edited tests and optimized functionality class

--- server/src/main/java/com/scoutress/KaimuxAdminStats/Services/DataFilter.java
+++ server/src/main/java/com/scoutress/KaimuxAdminStats/Services/DataFilter.java
@@ -6,25 +6,17 @@ import java.util.stream.Collectors;
 import com.scoutress.KaimuxAdminStats.Entity.DataItem;
 
 public class DataFilter {
-  public static List<DataItem> sessionsFilterByAid(List<DataItem> dataList, short aid) {
-    return dataList
+  public static List<DataItem> sessionsFilterByAid(List<DataItem> data, short aid) {
+    return data
         .stream()
         .filter(item -> item.getAid() == aid)
         .collect(Collectors.toList());
   }
 
-  public static List<DataItem> filterByAction(List<DataItem> dataList, boolean action) {
-    return dataList
+  public static List<DataItem> filterByAction(List<DataItem> data, boolean action) {
+    return data
         .stream()
         .filter(item -> item.isAction() == action)
         .collect(Collectors.toList());
-  }
-
-  public static List<DataItem> filterLogins(List<DataItem> data) {
-    return data.stream().filter(item -> item.isAction()).collect(Collectors.toList());
-  }
-
-  public static List<DataItem> filterLogouts(List<DataItem> data) {
-    return data.stream().filter(item -> !item.isAction()).collect(Collectors.toList());
   }
 }
--- server/src/test/java/com/scoutress/KaimuxAdminStats/ServicesImpl/NEW_DataFilterTest.java
+++ server/src/test/java/com/scoutress/KaimuxAdminStats/ServicesImpl/NEW_DataFilterTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 
 import com.scoutress.KaimuxAdminStats.Entity.DataItem;
 import com.scoutress.KaimuxAdminStats.Services.DataFilter;
-import com.scoutress.KaimuxAdminStats.Services.DataProcessing;
 
 public class NEW_DataFilterTest {
 
@@ -18,12 +17,19 @@ public class NEW_DataFilterTest {
 
     List<DataItem> dataList = Arrays.asList(
         new DataItem(1, (short) 10, time, true),
-        new DataItem(2, (short) 20, time, false),
-        new DataItem(3, (short) 10, time, true));
+        new DataItem(2, (short) 20, time, true),
+        new DataItem(3, (short) 10, time, false),
+        new DataItem(4, (short) 20, time, false),
+        new DataItem(5, (short) 15, time, true),
+        new DataItem(6, (short) 15, time, false),
+        new DataItem(7, (short) 75, time, true),
+        new DataItem(8, (short) 75, time, false),
+        new DataItem(9, (short) 20, time, true),
+        new DataItem(10, (short) 100, time, true));
 
     List<DataItem> expected = Arrays.asList(
         new DataItem(1, (short) 10, time, true),
-        new DataItem(3, (short) 10, time, true));
+        new DataItem(3, (short) 10, time, false));
 
     List<DataItem> result = DataFilter.sessionsFilterByAid(dataList, (short) 10);
 
@@ -38,35 +44,23 @@ public class NEW_DataFilterTest {
         new DataItem(1, (short) 10, time, true),
         new DataItem(2, (short) 10, time, false),
         new DataItem(3, (short) 10, time, true),
-        new DataItem(4, (short) 20, time, false));
+        new DataItem(4, (short) 10, time, false),
+        new DataItem(5, (short) 10, time, true),
+        new DataItem(6, (short) 10, time, false),
+        new DataItem(7, (short) 75, time, true),
+        new DataItem(8, (short) 10, time, false),
+        new DataItem(9, (short) 10, time, true),
+        new DataItem(10, (short) 20, time, false));
 
     List<DataItem> adminSessions = DataFilter.sessionsFilterByAid(dataList, (short) 10);
 
     List<DataItem> expectedLogins = Arrays.asList(
         new DataItem(1, (short) 10, time, true),
-        new DataItem(3, (short) 10, time, true));
+        new DataItem(3, (short) 10, time, true),
+        new DataItem(5, (short) 10, time, true),
+        new DataItem(9, (short) 10, time, true));
     List<DataItem> actualLogins = DataFilter.filterByAction(adminSessions, true);
 
     assertEquals(expectedLogins, actualLogins);
-  }
-
-  @Test
-  public void testSessionDuration() {
-    List<DataItem> dataList = Arrays.asList(
-        new DataItem(1, (short) 10, 1698578400, true),
-        new DataItem(2, (short) 10, 1698578460, false),
-        new DataItem(3, (short) 10, 1698578500, true),
-        new DataItem(4, (short) 10, 1698578560, false));
-
-    List<DataItem> logins = DataFilter.filterLogins(DataFilter.sessionsFilterByAid(dataList, (short) 10));
-    List<DataItem> logouts = DataFilter.filterLogouts(DataFilter.sessionsFilterByAid(dataList, (short) 10));
-
-    Long firstLoginTime = DataProcessing.getFirstLoginTime(logins);
-    Long firstLogoutTime = DataProcessing.getFirstLogoutTime(logouts);
-
-    long sessionDuration = DataProcessing.calculateSessionDuration(firstLoginTime, firstLogoutTime);
-
-    assertEquals(60, sessionDuration);
-    assert firstLoginTime < firstLogoutTime;
   }
 }

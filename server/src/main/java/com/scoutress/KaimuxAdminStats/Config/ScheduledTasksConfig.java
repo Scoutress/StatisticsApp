c0b106b0178b4c512b7bc641c3e5b5dc93f156da
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-11-16 18:57:09 +0200

    Implemented Discord ticket duplicates remover

--- server/src/main/java/com/scoutress/KaimuxAdminStats/Config/ScheduledTasksConfig.java
+++ server/src/main/java/com/scoutress/KaimuxAdminStats/Config/ScheduledTasksConfig.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import com.scoutress.KaimuxAdminStats.services.DataFetchingService;
+import com.scoutress.KaimuxAdminStats.servicesimpl.discordTickets.DiscordTicketsServiceImpl;
 
 import jakarta.transaction.Transactional;
 
@@ -16,23 +17,57 @@ import jakarta.transaction.Transactional;
 public class ScheduledTasksConfig {
 
   private final DataFetchingService dataFetchingService;
+  private final DiscordTicketsServiceImpl discordTicketsServiceImpl;
 
-  public ScheduledTasksConfig(DataFetchingService dataFetchingService) {
+  public ScheduledTasksConfig(
+      DataFetchingService dataFetchingService,
+      DiscordTicketsServiceImpl discordTicketsServiceImpl) {
     this.dataFetchingService = dataFetchingService;
+    this.discordTicketsServiceImpl = discordTicketsServiceImpl;
   }
 
   @Scheduled(initialDelay = 1000, fixedRate = 86400000)
   @Transactional
-  public void runDataExtractionFromAPI() {
-    System.out.println("Scheduled tasks started at: " + getCurrentTimestamp());
+  public void runScheduledTasks() {
+    System.out.println("-----------------------------------------------");
+    System.out.println("Started scheduled tasks at: " + getCurrentTimestamp());
     System.out.println("");
 
-    dataFetchingService.fetchAndSaveData(1, "discord");
-    dataFetchingService.fetchAndSaveData(1, "minecraft");
-    dataFetchingService.fetchAndSaveData(1, "visitors");
+    // runDiscordDataExtractionFromAPI();
+    // runMinecraftDataExtractionFromAPI();
+    // runVisitorsDataExtractionFromAPI();
+    // runDuplicateRemover();
 
     System.out.println("Scheduled tasks completed at: " + getCurrentTimestamp());
-    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    System.out.println("-----------------------------------------------");
+  }
+
+  private void runDiscordDataExtractionFromAPI() {
+    System.out.println("Running: runDiscordDataExtractionFromAPI");
+    dataFetchingService.fetchAndSaveData(1, "discord");
+    System.out.println("Completed: runDiscordDataExtractionFromAPI");
+    System.out.println("");
+  }
+
+  private void runMinecraftDataExtractionFromAPI() {
+    System.out.println("Running: runMinecraftDataExtractionFromAPI");
+    dataFetchingService.fetchAndSaveData(1, "minecraft");
+    System.out.println("Completed: runMinecraftDataExtractionFromAPI");
+    System.out.println("");
+  }
+
+  private void runVisitorsDataExtractionFromAPI() {
+    System.out.println("Running: runVisitorsDataExtractionFromAPI");
+    dataFetchingService.fetchAndSaveData(1, "visitors");
+    System.out.println("Completed: runVisitorsDataExtractionFromAPI");
+    System.out.println("");
+  }
+
+  private void runDuplicateRemover() {
+    System.out.println("Running: runDuplicateRemover");
+    discordTicketsServiceImpl.removeDuplicateReactions();
+    System.out.println("Completed: runDuplicateRemover");
+    System.out.println("");
   }
 
   private String getCurrentTimestamp() {
--- server/src/main/java/com/scoutress/KaimuxAdminStats/servicesimpl/discordTickets/DiscordTicketsServiceImpl.java
+++ server/src/main/java/com/scoutress/KaimuxAdminStats/servicesimpl/discordTickets/DiscordTicketsServiceImpl.java
@@ -10,131 +10,158 @@ import org.springframework.stereotype.Service;
 import com.scoutress.KaimuxAdminStats.entity.discordTickets.DiscordTickets;
 import com.scoutress.KaimuxAdminStats.entity.discordTickets.DiscordTicketsReactions;
 import com.scoutress.KaimuxAdminStats.entity.employees.EmployeeCodes;
+import com.scoutress.KaimuxAdminStats.repositories.discordTickets.DiscordTicketsReactionsRepository;
 import com.scoutress.KaimuxAdminStats.repositories.discordTickets.DiscordTicketsRepository;
 import com.scoutress.KaimuxAdminStats.services.DataExtractingService;
 import com.scoutress.KaimuxAdminStats.services.discordTickets.DiscordTicketsService;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class DiscordTicketsServiceImpl implements DiscordTicketsService {
 
-        public final DataExtractingService dataExtractingService;
-        public final DiscordTicketsRepository discordTicketsRepository;
+  public final DataExtractingService dataExtractingService;
+  public final DiscordTicketsRepository discordTicketsRepository;
+  public final DiscordTicketsReactionsRepository discordTicketsReactionsRepository;
 
-        public DiscordTicketsServiceImpl(
-                        DataExtractingService dataExtractingService,
-                        DiscordTicketsRepository discordTicketsRepository) {
-                this.dataExtractingService = dataExtractingService;
-                this.discordTicketsRepository = discordTicketsRepository;
-        }
+  public DiscordTicketsServiceImpl(
+      DataExtractingService dataExtractingService,
+      DiscordTicketsRepository discordTicketsRepository,
+      DiscordTicketsReactionsRepository discordTicketsReactionsRepository) {
+    this.dataExtractingService = dataExtractingService;
+    this.discordTicketsRepository = discordTicketsRepository;
+    this.discordTicketsReactionsRepository = discordTicketsReactionsRepository;
+  }
 
-        @Override
-        public void convertDiscordTicketsResponses() {
+  @Override
+  public void convertDiscordTicketsResponses() {
 
-                List<EmployeeCodes> employeeCodes = extractEmployeeCodes();
-                List<DiscordTicketsReactions> ticketsReactions = extractDataFromResponsesTable();
-                List<DiscordTicketsReactions> reactionsWithEmployeeIds = mapDiscordToEmployeeIds(ticketsReactions,
-                                employeeCodes);
-                List<DiscordTickets> convertedData = convertData(reactionsWithEmployeeIds);
+    List<EmployeeCodes> employeeCodes = extractEmployeeCodes();
+    List<DiscordTicketsReactions> ticketsReactions = extractDataFromResponsesTable();
+    List<DiscordTicketsReactions> reactionsWithEmployeeIds = mapDiscordToEmployeeIds(ticketsReactions,
+        employeeCodes);
+    List<DiscordTickets> convertedData = convertData(reactionsWithEmployeeIds);
 
-                saveDataToNewTable(convertedData);
-        }
+    saveDataToNewTable(convertedData);
+  }
 
-        public List<EmployeeCodes> extractEmployeeCodes() {
-                List<EmployeeCodes> data = dataExtractingService.getAllEmployeeCodes();
-                return data;
-        }
+  public List<EmployeeCodes> extractEmployeeCodes() {
+    List<EmployeeCodes> data = dataExtractingService.getAllEmployeeCodes();
+    return data;
+  }
 
-        public List<DiscordTicketsReactions> extractDataFromResponsesTable() {
-                List<DiscordTicketsReactions> data = dataExtractingService.getAllDcTicketReactions();
-                return data;
-        }
+  public List<DiscordTicketsReactions> extractDataFromResponsesTable() {
+    List<DiscordTicketsReactions> data = dataExtractingService.getAllDcTicketReactions();
+    return data;
+  }
 
-        public List<DiscordTickets> convertData(List<DiscordTicketsReactions> rawData) {
-                Map<Long, Map<LocalDate, Long>> groupedData = rawData
-                                .stream()
-                                .collect(Collectors
-                                                .groupingBy(
-                                                                DiscordTicketsReactions::getDiscordId,
-                                                                Collectors
-                                                                                .groupingBy(
-                                                                                                reaction -> reaction
-                                                                                                                .getDateTime()
-                                                                                                                .toLocalDate(),
-                                                                                                Collectors
-                                                                                                                .mapping(
-                                                                                                                                DiscordTicketsReactions::getTicketId,
-                                                                                                                                Collectors
-                                                                                                                                                .toSet()))))
-                                .entrySet()
-                                .stream()
-                                .collect(Collectors
-                                                .toMap(
-                                                                Map.Entry::getKey,
-                                                                entry -> entry
-                                                                                .getValue()
-                                                                                .entrySet()
-                                                                                .stream()
-                                                                                .collect(Collectors
-                                                                                                .toMap(
-                                                                                                                Map.Entry::getKey,
-                                                                                                                e -> (long) e
-                                                                                                                                .getValue()
-                                                                                                                                .size()))));
+  public List<DiscordTickets> convertData(List<DiscordTicketsReactions> rawData) {
+    Map<Long, Map<LocalDate, Long>> groupedData = rawData
+        .stream()
+        .collect(Collectors
+            .groupingBy(
+                DiscordTicketsReactions::getDiscordId,
+                Collectors
+                    .groupingBy(
+                        reaction -> reaction
+                            .getDateTime()
+                            .toLocalDate(),
+                        Collectors
+                            .mapping(
+                                DiscordTicketsReactions::getTicketId,
+                                Collectors
+                                    .toSet()))))
+        .entrySet()
+        .stream()
+        .collect(Collectors
+            .toMap(
+                Map.Entry::getKey,
+                entry -> entry
+                    .getValue()
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors
+                        .toMap(
+                            Map.Entry::getKey,
+                            e -> (long) e
+                                .getValue()
+                                .size()))));
 
-                return groupedData
-                                .entrySet()
-                                .stream()
-                                .flatMap(
-                                                adminEntry -> adminEntry
-                                                                .getValue()
-                                                                .entrySet()
-                                                                .stream()
-                                                                .map(
-                                                                                dateEntry -> new DiscordTickets(
-                                                                                                null,
-                                                                                                adminEntry
-                                                                                                                .getKey(),
-                                                                                                String
-                                                                                                                .valueOf(dateEntry
-                                                                                                                                .getValue()),
-                                                                                                dateEntry
-                                                                                                                .getKey())))
-                                .collect(Collectors
-                                                .toList());
-        }
+    return groupedData
+        .entrySet()
+        .stream()
+        .flatMap(
+            adminEntry -> adminEntry
+                .getValue()
+                .entrySet()
+                .stream()
+                .map(
+                    dateEntry -> new DiscordTickets(
+                        null,
+                        adminEntry
+                            .getKey(),
+                        String
+                            .valueOf(dateEntry
+                                .getValue()),
+                        dateEntry
+                            .getKey())))
+        .collect(Collectors
+            .toList());
+  }
 
-        public List<DiscordTicketsReactions> mapDiscordToEmployeeIds(
-                        List<DiscordTicketsReactions> reactions,
-                        List<EmployeeCodes> employeeCodes) {
+  public List<DiscordTicketsReactions> mapDiscordToEmployeeIds(
+      List<DiscordTicketsReactions> reactions,
+      List<EmployeeCodes> employeeCodes) {
 
-                Map<Short, Short> discordToEmployeeMap = employeeCodes
-                                .stream()
-                                .filter(code -> code.getDiscordId() != null)
-                                .filter(code -> code.getEmployeeId() != null)
-                                .collect(Collectors
-                                                .toMap(
-                                                                EmployeeCodes::getDiscordId,
-                                                                EmployeeCodes::getEmployeeId));
+    Map<Short, Short> discordToEmployeeMap = employeeCodes
+        .stream()
+        .filter(code -> code.getDiscordId() != null)
+        .filter(code -> code.getEmployeeId() != null)
+        .collect(Collectors
+            .toMap(
+                EmployeeCodes::getDiscordId,
+                EmployeeCodes::getEmployeeId));
 
-                return reactions
-                                .stream()
-                                .map(reaction -> {
-                                        Short employeeId = discordToEmployeeMap
-                                                        .get(reaction
-                                                                        .getDiscordId()
-                                                                        .shortValue());
-                                        if (employeeId != null) {
-                                                reaction
-                                                                .setDiscordId(employeeId
-                                                                                .longValue());
-                                        }
-                                        return reaction;
-                                })
-                                .collect(Collectors.toList());
-        }
+    return reactions
+        .stream()
+        .map(reaction -> {
+          Short employeeId = discordToEmployeeMap
+              .get(reaction
+                  .getDiscordId()
+                  .shortValue());
+          if (employeeId != null) {
+            reaction
+                .setDiscordId(employeeId
+                    .longValue());
+          }
+          return reaction;
+        })
+        .collect(Collectors.toList());
+  }
 
-        private void saveDataToNewTable(List<DiscordTickets> convertedData) {
-                convertedData.sort((a, b) -> a.getDate().compareTo(b.getDate()));
-                convertedData.forEach(discordTicketsRepository::save);
-        }
+  private void saveDataToNewTable(List<DiscordTickets> convertedData) {
+    convertedData.sort((a, b) -> a.getDate().compareTo(b.getDate()));
+    convertedData.forEach(discordTicketsRepository::save);
+  }
+
+  @Transactional
+  public void removeDuplicateReactions() {
+    List<DiscordTicketsReactions> allReactions = discordTicketsReactionsRepository.findAll();
+
+    Map<List<Object>, List<Long>> groupedReactions = allReactions.stream()
+        .collect(Collectors.groupingBy(
+            reaction -> List.of(
+                reaction.getDiscordId(),
+                reaction.getTicketId()),
+            Collectors.mapping(
+                DiscordTicketsReactions::getId,
+                Collectors.toList())));
+
+    List<Long> duplicateIds = groupedReactions.values()
+        .stream()
+        .flatMap(ids -> ids.stream().skip(1))
+        .collect(Collectors.toList());
+
+    discordTicketsReactionsRepository.deleteAllById(duplicateIds);
+  }
 }

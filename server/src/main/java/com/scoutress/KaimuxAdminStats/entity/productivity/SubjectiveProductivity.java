823e3f49c862564ca6f77f4309f0758ec314cde8
Scoutress <142579732+Scoutress@users.noreply.github.com>
2024-11-16 19:57:56 +0200

    Created entities for objective and subjective productivities

---
+++ server/src/main/java/com/scoutress/KaimuxAdminStats/entity/productivity/SubjectiveProductivity.java
@@ -0,0 +1,29 @@
+package com.scoutress.KaimuxAdminStats.entity.productivity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "subjective_productivity")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubjectiveProductivity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Short id;
+
+  @Column(name = "aid", nullable = false, unique = true)
+  private Short aid;
+
+  @Column(name = "value", nullable = false, unique = true)
+  private Double value;
+}

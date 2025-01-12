7927a975a5285ca690677f4e5f2f571c14e4895f
Scoutress <142579732+Scoutress@users.noreply.github.com>
2025-01-12 16:52:08 +0200

    Added links to each employee main stats page

--- client/src/pages/home/HomePage.jsx
+++ client/src/pages/home/HomePage.jsx
@@ -1,13 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import styles from "./HomePage.module.scss";
 
 const Homepage = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/employee/all");
+        setEmployees(response.data);
+      } catch (error) {
+        console.error("Error fetching employees:", error);
+      }
+    };
+    fetchEmployees();
+  }, []);
+
   return (
     <div className={styles.homePage}>
       <div className={styles.category}>
         <h2>Productivity</h2>
         <div className={styles.linkGroup}>
           <Link to="/stats/productivity">Productivity</Link>
+        </div>
+      </div>
+
+      <div className={styles.category}>
+        <h2>Main stats (what employee can see)</h2>
+        <div className={styles.linkGroup}>
+          {employees.map((employee) => (
+            <Link
+              key={employee.id}
+              to={`/user/stats/${employee.id}`}
+              className={styles.employeeButton}
+            >
+              {employee.username} main stats
+            </Link>
+          ))}
         </div>
       </div>
 
